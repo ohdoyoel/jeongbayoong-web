49ba649b45771e64aa0193c7df9d2a800991239d
ohdoyoel <ohdoyoel@naver.com>
2025-11-03 17:06:29 +0900

    Update page layout and styles: removed default content, added new headings and a link to the blog, and modified background color. Commented out foreground color in globals.css for dark mode.

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex flex-col min-h-screen w-full py-32 px-16 bg-blue-500">
+        <h1 className="text-4xl font-bold">블로그입니다.</h1>
+      </main>
+    </div>
+  );
+}
